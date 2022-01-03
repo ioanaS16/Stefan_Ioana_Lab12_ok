fb59e67fbbfe6f97ca7c05c6beb3788a79812f17
ioanaS16 <stefanioana16@gmail.com>
2022-01-03 15:41:54 +0200

    Add project files.

---
+++ App_Xamarin_Lab12/App_Xamarin_Lab12/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using App_Xamarin_Lab12.Data;
+using Xamarin.Forms;
+
+namespace App_Xamarin_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            InitializeComponent();
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Data/Stefan_Ioana_Lab12Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Stefan_Ioana_Lab12.Models;
+
+namespace Stefan_Ioana_Lab12.Data
+{
+    public class Stefan_Ioana_Lab12Context : DbContext
+    {
+        public Stefan_Ioana_Lab12Context (DbContextOptions<Stefan_Ioana_Lab12Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Stefan_Ioana_Lab12.Models.ShopList> ShopList { get; set; }
+    }
+}
